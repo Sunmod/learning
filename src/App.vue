e62885f456aa0e52b57463ba77bc6e08f25732d2
Stepan Turbasov <kaskader2001@mail.ru>
2021-12-24 15:19:36 +0300

    Vue 2
    start course vue 2
    
    The Vue Instance
    how to use Vue to display data onto a webpage
    
    Add a description key to our existing data object with the value “A pair of warm, fuzzy socks”. Then display description using an expression in an p element, underneath the h1.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
 <!--    <json-milavitsa :json="json"/>-->
+    <fundamental/>
   </div>
 </template>
 
 <script>
 
 import json from './data'
+import Fundamental from "./components/vuemastery/fundamental";
 // import JsonMilavitsa from "./components/JSON_milavitsa";
 
 export default {
   name: 'App',
   components: {
+    Fundamental
     // JsonMilavitsa
   },
   data() {
---
+++ src/components/vuemastery/fundamental.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <h1>{{ product }}</h1>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "fundamental",
+  data() {
+    return {
+      product: 'Socks',
+      description: 'A pair of warm, fuzzy socks'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
