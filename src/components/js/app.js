68fc57e50ff97dc1be50ae8df8e15aad2fe550b8
Stepan Turbasov <kaskader2001@mail.ru>
2021-12-30 17:43:03 +0300

    JS 2 (function)
    изменить функцией значение макс скорости

--- src/components/js/app.js
+++ src/components/js/app.js
@@ -1,5 +1,5 @@
 let ford = {
-    color: 'green',
+    color: 'blue',
     maxSpeed: 250,
     audio: {
         brand: 'Yamaha',
@@ -7,17 +7,48 @@ let ford = {
     },
     cost: 4000000,
 };
+let bmw = {
+    color: 'yellow',
+};
 
+
+// /*
+// дз изменить значение макс скорости через функцию
+console.log('Первоначальная макс скорость: ' + ford.maxSpeed)
+speedChange(ford)
+function speedChange(theCar) {
+    theCar.maxSpeed = 300
+    console.log('Измененная макс скорость:' + ford.maxSpeed)
+}
+// */
+
+/*
+знакомство с функциями
+colorChange(ford)
+colorChange(bmw)
+function colorChange(theCar) {
+    theCar.color = 'red';
+}
+console.log(ford.color)
+console.log(bmw.color)
+ */
+
+/*
+дз if
 ford.cost > 1000000  ? console.log('Дорогой'):console.log('Не дорогой');
+*/
 
-// let a = 2;
-// let b = `test`;
-// let c = `Ел сегодня ${a} раза`;
-// let d = `Ел сегодня ` + a + ` раза`;
-// let e = true;
-// let f = null;
-// let g = undefined;
-// console.log(a, b, e, f, g);
-// console.log(typeof(a, b, e, f, g));
-// console.log(c);
-// console.log(d);
+/*
+переменные
+let a = 2;
+let b = `test`;
+let c = `Ел сегодня ${a} раза`;
+let d = `Ел сегодня ` + a + ` раза`;
+let e = true;
+let f = null;
+let g = undefined;
+console.log(a, b, e, f, g);
+console.log(typeof(a, b, e, f, g));
+console.log(c);
+console.log(d);
+*/
