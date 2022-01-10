2ea2455811e4e4f704c999ddc1f86803dcc85302
Stepan Turbasov <kaskader2001@mail.ru>
2022-01-10 17:05:57 +0300

    JS 7 (dom, html)
    Поиск по селектору

--- src/components/js/js/dom_and_html.js
+++ src/components/js/js/dom_and_html.js
@@ -1,3 +1,5 @@
-document.getElementById('header').innerText = 'test'
+document.getElementById('header').innerText = 'Измененный заголовок'
+let findElement = document.querySelector('.myClass')
 console.log(document.getElementById('header'))
 console.log(typeof  document.getElementById('header'))
+console.log(findElement)
