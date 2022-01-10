26a71ec4f41c06ce50837a5f9510be0eccf22a70
Stepan Turbasov <kaskader2001@mail.ru>
2022-01-10 16:52:09 +0300

    JS 6 (objects and classes)
    Дз вывести запись типа
    Канал (название) [ автор: (имя) (фамилия) ]

--- src/components/js/js/objects_and_classes.js
+++ src/components/js/js/objects_and_classes.js
@@ -10,3 +10,17 @@ class  Hater {
 
 let hater1 = new Hater('Ronald', 'Tomskiy');
 hater1.sayName()
+
+class Bloger {
+    constructor(channelName, firstName, lastName) {
+        this.channelName = channelName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    isBloger () {
+        console.log(`Канал ${this.channelName}` + ' [ автор канала: ' + `${this.firstName} ${this.lastName}` + ' ]')
+    }
+}
+
+let bloger1 = new Bloger('Trent Channel', 'Stepan', 'Turbasov')
+bloger1.isBloger()
